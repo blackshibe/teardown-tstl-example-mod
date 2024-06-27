de493457fc4181dff2ecffe423710e32913ffed6
blackshibe <kacperszczepan@tutanota.com>
2024-06-27 02:59:45 +0200

    fix submodules again

--- src/include/types/api/debug.d.ts
+++
@@ -1,8 +0,0 @@
-/*
-    Typings for Teardown Lua API 1.5
-    by blackshibe, written 26/06/2024
-*/
-
-declare function DebugPrint(this: void, data: string): void;
-
-declare function DebugLine(this: void, start: TVec, end: TVec, r?: number, g?: number, b?: number): void;
